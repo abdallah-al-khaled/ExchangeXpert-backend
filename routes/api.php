87e5e81786cc857afbec1a413cbc80792f425051
Abdallah <fafafavd@gmail.com>
2024-08-26 12:41:43 +0300

    Added refresh API routes

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,5 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
 });
