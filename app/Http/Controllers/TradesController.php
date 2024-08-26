f96d2f04c697ae745dd8411cecd5b96d3bab5691
Abdallah <fafafavd@gmail.com>
2024-08-26 21:26:08 +0300

    Added index function for trades controller

--- app/Http/Controllers/TradesController.php
+++ app/Http/Controllers/TradesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trade;
 use Illuminate\Http\Request;
 
 class TradesController extends Controller
 {
-    //
+    public function index()
+    {
+        $trades = Trade::with('userBot')->get();
+        return response()->json($trades);
+    }
 }
