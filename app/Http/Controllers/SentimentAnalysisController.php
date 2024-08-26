bb4ecc83304465b98cb84b327c872398452bb427
Abdallah <fafafavd@gmail.com>
2024-08-26 21:31:27 +0300

    Added index function for sentiment analysis controller

--- app/Http/Controllers/SentimentAnalysisController.php
+++ app/Http/Controllers/SentimentAnalysisController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SentimentAnalysis;
 use Illuminate\Http\Request;
 
 class SentimentAnalysisController extends Controller
 {
-    //
+    public function index()
+    {
+        $analyses = SentimentAnalysis::all();
+        return response()->json($analyses);
+    }
 }
