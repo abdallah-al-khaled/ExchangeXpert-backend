af3cec634a5ba0b38b5fc7cdc3974dc64a6eaf95
Abdallah <fafafavd@gmail.com>
2024-08-26 11:40:34 +0300

    Added logout function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -66,4 +66,13 @@ class AuthController extends Controller
             ]
         ]);
     }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
 }
