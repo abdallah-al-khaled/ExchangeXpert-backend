33caeb96438938d13419094d106fe4579142cdf8
Abdallah <fafafavd@gmail.com>
2024-08-26 18:47:46 +0300

    Added index function for userbots controller

--- app/Http/Controllers/UserBotsController.php
+++ app/Http/Controllers/UserBotsController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class UserBotsController extends Controller
 {
-    //
+    public function index()
+    {
+        $userBots = UserBot::with('user', 'bot')->get();
+        return response()->json($userBots);
+    }
 }
