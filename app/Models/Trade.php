96e18ac733c23d44be6878a68f4adb7892b375dd
Abdallah <fafafavd@gmail.com>
2024-08-26 17:17:28 +0300

    Added fillable in trade model

--- app/Models/Trade.php
+++ app/Models/Trade.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Trade extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_bot_id', 'stock_symbol', 'action', 'quantity', 'price', 
+        'buy_at', 'sold_at'
+    ];
 }
