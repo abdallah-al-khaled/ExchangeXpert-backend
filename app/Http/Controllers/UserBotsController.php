590db015b63a8f997f68d63ccbe5aa4cc8e4ad55
Abdallah <fafafavd@gmail.com>
2024-08-26 18:48:47 +0300

    Added update function for userbots controller

--- app/Http/Controllers/UserBotsController.php
+++ app/Http/Controllers/UserBotsController.php
@@ -23,4 +23,11 @@ class UserBotsController extends Controller
         $userBot = UserBot::with('user', 'bot')->findOrFail($id);
         return response()->json($userBot);
     }
+
+    public function update(Request $request, $id)
+    {
+        $userBot = UserBot::findOrFail($id);
+        $userBot->update($request->all());
+        return response()->json($userBot, 200);
+    }
 }
