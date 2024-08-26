68fee3cb7637c5c6291fd2823b35ba4858589ffa
Abdallah <fafafavd@gmail.com>
2024-08-26 21:38:09 +0300

    Added index function for ml prediction controller

--- app/Http/Controllers/MlPredictionsController.php
+++ app/Http/Controllers/MlPredictionsController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MlPrediction;
 use Illuminate\Http\Request;
 
 class MlPredictionsController extends Controller
 {
-    //
+    public function index()
+    {
+        $predictions = MlPrediction::all();
+        return response()->json($predictions);
+    }
+
 }
