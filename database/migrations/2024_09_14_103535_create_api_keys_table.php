fb07e0f6972d0bf31dbff7029d92c8d8f4e13329
Abdallah <fafafavd@gmail.com>
2024-09-14 13:39:01 +0300

    Link api_key to users table

--- database/migrations/2024_09_14_103535_create_api_keys_table.php
+++ database/migrations/2024_09_14_103535_create_api_keys_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('api_keys', function (Blueprint $table) {
             $table->id();
+                        $table->foreignId('user_id')->constrained()->onDelete('cascade');  // Link to users table
             $table->timestamps();
         });
     }
